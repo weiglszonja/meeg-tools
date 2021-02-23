d12d74be12e5af4e8a009c2b3d71a41bd325df3e
weiglszonja <wszonja@gmail.com>
2021-02-23 17:57:57 +0100

    update repo

---
+++ eeg_preprocessing/run_preprocessing.py
@@ -0,0 +1,157 @@
+import os
+import mne
+from autoreject import autoreject, Ransac
+
+from .utils.faster import faster_bad_epochs
+import numpy as np
+import pandas as pd
+from random import sample
+
+
+def concat_raws_from_events(events: pd.DataFrame, raw: mne.io.Raw) -> mne.io.Raw:
+    raws = []
+    for idx, event in events.iterrows():
+        if 'real_start_time' in event:
+            raws.append(raw.copy().crop(tmin=event['real_start_time'] + 5, tmax=event['end_time'], include_tmax=True))
+        else:
+            raws.append(raw.copy().crop(tmin=event['start_time'], tmax=event['end_time'], include_tmax=True))
+    return mne.concatenate_raws(raws)
+
+
+def run_ica(raw: mne.io.Raw) -> mne.Epochs:
+    sfreq = raw.info['sfreq']
+    # remove slow drifts and high freq noise
+    raw_bandpass_ica = raw.load_data().copy().filter(l_freq=0.5, h_freq=45)
+
+    # create fixed length epoch for preprocessing
+    epoch_duration_in_seconds = 1.0
+
+    events = mne.make_fixed_length_events(raw_bandpass_ica,
+                                          id=1,
+                                          first_samp=True,
+                                          duration=epoch_duration_in_seconds)
+
+    epochs = mne.Epochs(raw=raw_bandpass_ica,
+                        events=events,
+                        picks='eeg',
+                        event_id=1,
+                        baseline=None,
+                        tmin=0.,
+                        tmax=epoch_duration_in_seconds - (1 / sfreq),
+                        preload=True)
+
+    # remove from memory
+    del raw_bandpass_ica
+
+    print('Preliminary epoch rejection: ')
+    bad_epochs = faster_bad_epochs(epochs, picks=None, thres=3, use_metrics=None)
+    epochs_faster = epochs.drop(bad_epochs, reason='FASTER')
+
+    del epochs
+
+    # run ica
+    n_components = 32
+    method = 'infomax'
+
+    ica = mne.preprocessing.ICA(n_components=n_components, random_state=42, method=method)
+    ica.fit(epochs_faster, decim=2)
+
+    epochs_faster.set_channel_types({'Fp1': 'eog', 'Fp2': 'eog'})
+    eog_indices, eog_scores = ica.find_bads_eog(epochs_faster)
+    ica.exclude = eog_indices
+    ica.plot_sources(epochs_faster, start=0, stop=10)
+
+    ica.apply(epochs_faster)
+
+    epochs_faster.info['ica_method'] = method
+    epochs_faster.info['ica_n_components'] = n_components
+    epochs_faster.info['ica_n_excluded'] = len(ica.exclude)
+
+    return epochs_faster
+
+
+def run_autoreject(epochs: mne.Epochs, n_jobs: int = 11, subset: bool = False) -> mne.Epochs:
+    # run autoreject on epochs
+    ar = autoreject.AutoReject(random_state=42, n_jobs=n_jobs)
+
+    if subset:
+        n_epochs = len(epochs)
+        print('Fitting autoreject on random subset of epochs: ')
+        subset = sample(set(np.arange(0, n_epochs, 1)), int(n_epochs * 0.25))
+        ar.fit(epochs[subset])
+
+    else:
+        print('Fitting autoreject on epochs: ')
+        ar.fit(epochs)
+
+    reject_log = ar.get_reject_log(epochs)
+    # epochs[reject_log.bad_epochs].plot(n_epochs=10, title='ALL BAD EPOCHS AUTOREJECT')
+
+    reject_df = pd.DataFrame(data=reject_log.labels, columns=epochs.info['ch_names'])
+    # 0 : good data segment
+    # 1 : bad data segment not interpolated
+    # 2 : bad data segment interpolated
+    reject_df = reject_df.replace(1, 0)
+    reject_df = reject_df.replace(2, 1)
+    reject_df = reject_df.replace(np.nan, 0)
+
+    # mark channels where more than 50% of epochs were marked as bad channels
+    bad_channels = reject_df.astype(bool).sum(axis=0) > len(epochs) / 2
+    bads = bad_channels.index[bad_channels].tolist()
+
+    epochs.drop(reject_log.bad_epochs, reason='AUTOREJECT')
+    epochs.info['bad_channels_autoreject'] = bads
+
+    return epochs
+
+
+def run_ransac(epochs: mne.Epochs, n_jobs: int = 11) -> mne.Epochs:
+    # find bad channels with ransac
+    ransac = Ransac(verbose='progressbar', n_jobs=n_jobs)
+    epochs_ransac = ransac.fit_transform(epochs)
+    epochs_ransac.info['bad_channels_ransac'] = ransac.bad_chs_
+
+    return epochs_ransac
+
+#
+# if __name__ == '__main__':
+#     # Set base path to EEG data
+#     base_path = 'G:/TMS_rewiring/'
+#     eeg_path = os.path.join(base_path, 'Raw_data/24_L/Day1/EEG/')
+#     # Create folder for preprocessed and interim files
+#     folder_name = 'preprocessed'
+#     interim_path = os.path.join(base_path, folder_name)
+#
+#     raw_file_name = [file for file in os.listdir(eeg_path) if file.endswith('.vhdr')][0]
+#     subject, condition, day = raw_file_name.split('_')
+#     num_day = [d for d in day if d.isdigit()][0]
+#
+#     # read raw file (not loaded into memory)
+#     raw = mne.io.read_raw_brainvision(os.path.join(eeg_path, raw_file_name), preload=False, verbose=True)
+#
+#     resting_events, block_events = get_events_from_raw(raw)
+#
+#     # Create path to interim epoch files
+#     interim_epochs_path = os.path.join(interim_path, condition, 'epochs_rs')
+#     if not os.path.exists(interim_epochs_path):
+#         os.makedirs(interim_epochs_path)
+#
+#     # Create path to interim raw files
+#     interim_raw_path = os.path.join(interim_path, condition, 'raw_rs')
+#     if not os.path.exists(interim_raw_path):
+#         os.makedirs(interim_raw_path)
+#
+#     raw_rs = concat_raws_from_events(resting_events)
+#     epochs_ica = run_ica(raw=raw_rs)
+#
+#
+
+
+
+
+
+
+
+
+
+
---
+++ eeg_preprocessing/utils/events.py
@@ -0,0 +1,71 @@
+from pathlib import Path
+import mne
+import pandas as pd
+
+
+def get_events_from_raw(raw: mne.io.Raw) -> pd.DataFrame:
+    raw_file_name = Path(raw.filenames[0]).stem
+    subject, condition, day = raw_file_name.split('_')
+    num_day = [d for d in day if d.isdigit()][0]
+
+    # Read annotations from raw
+    events_from_annot, _ = mne.events_from_annotations(raw)
+
+    event_dict = {83: f'rs_{num_day}',
+                  91: f'asrt_{num_day}_1',
+                  93: f'asrt_{num_day}_2',
+                  95: f'asrt_{num_day}_3',
+                  97: f'asrt_{num_day}_4',
+                  99: f'asrt_{num_day}_5',
+                  101: f'asrt_{num_day}_6',
+                  87: f'rs_{int(num_day) + 1}'}
+
+    events_df = pd.DataFrame(data=events_from_annot, columns=['start_time', 'ignore', 'event_id']).drop(
+        columns=['ignore'])
+    events_df['start_time'] = events_df['start_time'] / raw.info['sfreq']
+    events_df['event'] = events_df['event_id'].map(event_dict).fillna(events_df['event_id'])
+
+    # blocks
+    blocks = events_df.index[events_df['event_id'].isin([91, 93, 95, 97, 99, 101])]
+    block_start_times = []
+    for block_num in range(len(blocks)):
+        block_start_index = blocks[block_num]
+        block_end_index = events_df.index[events_df['event_id'].isin([52, 152])][block_num]
+        block_df = events_df.iloc[block_start_index:block_end_index]
+        assert len(block_df[block_df['event_id'].isin([19, 119])]) == 7
+        real_block_start_time = block_df[block_df['event_id'].isin([19, 119])]['start_time'].values.tolist()[-1]
+        block_start_times.append(real_block_start_time)
+
+    block_events = events_df[events_df['event_id'].isin([91, 93, 95, 97, 99, 101])]
+    #block_events['event'] = block_events['event_id'].map(event_dict).fillna(block_events['event_id'])
+    block_events['real_start_time'] = block_start_times
+    if num_day != '3':
+        try:
+            # only seq A
+            block_events['end_time'] = events_df[events_df['event_id'] == 52]['start_time'].values
+            block_events['sequence'] = 'A'
+        except IndexError:
+            # only seq B
+            block_events['end_time'] = events_df[events_df['event_id'] == 152]['start_time'].values
+            block_events['sequence'] = 'B'
+    else:
+        # Az utolsó napon az 1-3-5 epochokban van az A és a 2-4-6 epochokban a B szekvencia a páratlan számú részvevőknél,
+        # és a párosoknál forditva.
+        if (int(subject) % 2) == 0:
+            block_events.loc[block_events['event_id'].isin([91, 95, 99]), 'sequence'] = 'B'
+            block_events.loc[block_events['event_id'].isin([93, 97, 101]), 'sequence'] = 'A'
+        else:
+            block_events.loc[block_events['event_id'].isin([91, 95, 99]), 'sequence'] = 'A'
+            block_events.loc[block_events['event_id'].isin([93, 97, 101]), 'sequence'] = 'B'
+
+        block_events['end_time'] = events_df[events_df['event_id'].isin([52, 152])]['start_time'].values
+
+    # Create slice from resting events
+    resting_events = events_df[events_df['event_id'].isin([83, 87])]
+    resting_end_times = events_df[events_df['event_id'].isin([84, 88])]['start_time'].values
+
+    resting_events['end_time'] = resting_end_times
+    #resting_events['event'] = resting_events['event_id'].map(event_dict).fillna(resting_events['event_id'])
+
+    return resting_events, block_events
+

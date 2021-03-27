ceaa68d0ba3dd21a3ec844d7cf8f5eddba500582
weiglszonja <wszonja@gmail.com>
2021-03-27 22:27:22 +0100

    Generalize preprocessing methods

--- eeg_preprocessing/preprocessing.py
+++ eeg_preprocessing/preprocessing.py
@@ -6,40 +6,18 @@ import numpy as np
 from random import sample
 
 
-def create_epochs_for_ica(raw: mne.io.Raw) -> mne.Epochs:
+def prepare_epochs_for_ica(epochs: mne.Epochs) -> mne.Epochs:
     """
-    Creates fixed length continuous epochs for ICA.
+    Prepares epochs for ICA:
     Drops epochs that were marked bad based on outlier detection.
     Parameters
     ----------
-    raw: the instance to be used for creating the epochs
+    epochs
 
     Returns
     -------
     Epochs instance
     """
-    # remove slow drifts and high freq noise
-    raw_bandpass = raw.load_data().copy().filter(l_freq=0.5, h_freq=45)
-
-    # create fixed length epoch for preprocessing
-    epoch_duration_in_seconds = 1.0
-    events = mne.make_fixed_length_events(raw_bandpass,
-                                          id=1,
-                                          first_samp=True,
-                                          duration=epoch_duration_in_seconds)
-
-    epochs = mne.Epochs(raw=raw_bandpass,
-                        events=events,
-                        picks='eeg',
-                        event_id=1,
-                        baseline=None,
-                        tmin=0.,
-                        tmax=epoch_duration_in_seconds - (1 / raw.info['sfreq']),
-                        preload=True)
-
-    # remove from memory
-    del raw_bandpass
-
     print('Preliminary epoch rejection: ')
     bad_epochs = faster_bad_epochs(epochs, picks=None, thres=3, use_metrics=None)
     epochs_faster = epochs.copy().drop(bad_epochs, reason='FASTER')
@@ -70,14 +48,13 @@ def run_ica(epochs: mne.Epochs) -> mne.preprocessing.ica:
     eog_indices, eog_scores = ica.find_bads_eog(epochs)
     ica.exclude = eog_indices
 
-    ica.plot_sources(epochs, start=0, stop=10)
-
     epochs.set_channel_types({'Fp1': 'eeg', 'Fp2': 'eeg'})
 
     return ica
 
 
-def run_autoreject(epochs: mne.Epochs, n_jobs: int = 11, subset: bool = False) -> mne.Epochs:
+def run_autoreject(epochs: mne.Epochs, n_jobs: int = 11,
+                   subset: bool = False) -> autoreject.AutoReject:
     """
     Drop bad epochs based on AutoReject.
     Parameters
@@ -88,25 +65,22 @@ def run_autoreject(epochs: mne.Epochs, n_jobs: int = 11, subset: bool = False) -
 
     Returns
     -------
-    Epochs instance
+    Autoreject instance
     """
     ar = autoreject.AutoReject(random_state=42, n_jobs=n_jobs)
 
+    n_epochs = len(epochs)
     if subset:
-        n_epochs = len(epochs)
-        print(f'Fitting autoreject on random (n={n_epochs} subset of epochs: ')
+
+        print(f'Fitting autoreject on random (n={int(n_epochs * 0.25)}) subset of epochs: ')
         subset = sample(set(np.arange(0, n_epochs, 1)), int(n_epochs * 0.25))
         ar.fit(epochs[subset])
 
     else:
-        print('Fitting autoreject on epochs: ')
+        print(f'Fitting autoreject on (n={n_epochs}) epochs: ')
         ar.fit(epochs)
 
-    reject_log = ar.get_reject_log(epochs)
-    epochs.drop(reject_log.bad_epochs, reason='AUTOREJECT')
-    epochs.info['reject_log'] = reject_log
-
-    return epochs
+    return ar
 
 
 def run_ransac(epochs: mne.Epochs, n_jobs: int = 11) -> mne.Epochs:
@@ -127,6 +101,6 @@ def run_ransac(epochs: mne.Epochs, n_jobs: int = 11) -> mne.Epochs:
     if ransac.bad_chs_:
         bads_str = ', '.join(ransac.bad_chs_)
         epochs_ransac.info.update(
-            description=epochs_ransac.info['description'] + ' interpolated: ' + bads_str)
+            description=epochs_ransac.info['description'] + ', interpolated: ' + bads_str)
 
     return epochs_ransac

e46ff50d26309c06c12af5106acae6a894ef6dfe
weiglszonja <wszonja@gmail.com>
2021-05-18 11:22:01 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+# Get requirements
+with open('requirements.txt') as f:
+    install_requires = f.read().strip().split('\n')
+
+setup_args = dict(
+    name='eeg-preprocessing',
+    version='0.1.0',
+    description='Semiautomatic framework for preprocessing EEG data',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Szonja Weigl',
+    author_email='weigl.anna.szonja@gmail.com',
+    url='https://github.com/weiglszonja/eeg-preprocessing',
+    packages=['eeg_preprocessing'],
+    install_requires=install_requires,
+)
+
+
+if __name__ == '__main__':
+    setup(**setup_args)

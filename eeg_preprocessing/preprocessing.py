9eef8e446909eb8fa93aca1889b96ad2e05b85a6
weiglszonja <wszonja@gmail.com>
2021-06-09 12:35:51 +0200

    update preprocessing.py

--- eeg_preprocessing/preprocessing.py
+++ eeg_preprocessing/preprocessing.py
@@ -1,17 +1,19 @@
-import mne
-from autoreject import autoreject, Ransac
-
-from .utils.faster import faster_bad_epochs
 import numpy as np
+from mne import Epochs
+from mne.preprocessing import bads, ICA
+from autoreject import autoreject, Ransac
 from random import sample
 
-from .utils.config import settings
+from utils.config import settings
+from mne.utils import logger
 
 
-def prepare_epochs_for_ica(epochs: mne.Epochs) -> mne.Epochs:
+def prepare_epochs_for_ica(epochs: Epochs) -> Epochs:
     """
-    Prepares epochs for ICA:
-    Drops epochs that were marked bad based on outlier detection.
+    Drops epochs that were marked bad based on a global outlier detection.
+    This implementation for the preliminary epoch rejection was based on the
+    Python implementation of the FASTER algorithm from Marijn van Vliet
+    https://gist.github.com/wmvanvliet/d883c3fe1402c7ced6fc
     Parameters
     ----------
     epochs
@@ -20,14 +22,37 @@ def prepare_epochs_for_ica(epochs: mne.Epochs) -> mne.Epochs:
     -------
     Epochs instance
     """
-    print('Preliminary epoch rejection: ')
-    bad_epochs = faster_bad_epochs(epochs, picks=None, thres=3, use_metrics=None)
+    logger.info('Preliminary epoch rejection: ')
+
+    def _deviation(data: np.ndarray) -> np.ndarray:
+        """
+        Computes the deviation from mean for each channel.
+        """
+        channels_mean = np.mean(data, axis=2)
+        return channels_mean - np.mean(channels_mean, axis=0)
+
+    metrics = {
+        'amplitude': lambda x: np.mean(np.ptp(x, axis=2), axis=1),
+        'deviation': lambda x: np.mean(_deviation(x), axis=1),
+        'variance': lambda x: np.mean(np.var(x, axis=2), axis=1),
+    }
+
+    epochs_data = epochs.get_data()
+
+    bad_epochs = []
+    for metric in metrics:
+        scores = metrics[metric](epochs_data)
+        outliers = bads._find_outliers(scores, threshold=3.0)
+        logger.info(f'Bad epochs by {metric}\n\t{outliers}')
+        bad_epochs.extend(outliers)
+
+    bad_epochs = list(set(bad_epochs))
     epochs_faster = epochs.copy().drop(bad_epochs, reason='FASTER')
 
     return epochs_faster
 
 
-def run_ica(epochs: mne.Epochs) -> mne.preprocessing.ica:
+def run_ica(epochs: Epochs) -> ICA:
     """
     Runs ICA decomposition on Epochs instance.
 
@@ -40,9 +65,9 @@ def run_ica(epochs: mne.Epochs) -> mne.preprocessing.ica:
     -------
     ICA instance
     """
-    ica = mne.preprocessing.ICA(n_components=settings['ica']['n_components'],
-                                random_state=42,
-                                method=settings['ica']['method'])
+    ica = ICA(n_components=settings['ica']['n_components'],
+              random_state=42,
+              method=settings['ica']['method'])
     ica.fit(epochs, decim=settings['ica']['decim'])
 
     if 'eog' not in epochs.get_channel_types():
@@ -56,7 +81,7 @@ def run_ica(epochs: mne.Epochs) -> mne.preprocessing.ica:
     return ica
 
 
-def run_autoreject(epochs: mne.Epochs, n_jobs: int = 11,
+def run_autoreject(epochs: Epochs, n_jobs: int = 11,
                    subset: bool = False) -> autoreject.AutoReject:
     """
     Drop bad epochs based on AutoReject.
@@ -86,7 +111,7 @@ def run_autoreject(epochs: mne.Epochs, n_jobs: int = 11,
     return ar
 
 
-def run_ransac(epochs: mne.Epochs, n_jobs: int = 11) -> mne.Epochs:
+def run_ransac(epochs: Epochs, n_jobs: int = 11) -> Epochs:
     """
     Find and interpolate bad channels with Ransac.
     If there are no bad channels found returns the Epochs instance unmodified.

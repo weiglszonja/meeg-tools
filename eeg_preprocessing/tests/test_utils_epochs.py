1d15cf2f8bebef9900605493a16bb3f82927bd87
weiglszonja <wszonja@gmail.com>
2021-06-17 11:33:28 +0200

    add unittests

---
+++ eeg_preprocessing/tests/test_utils_epochs.py
@@ -0,0 +1,78 @@
+import os
+import unittest
+
+import numpy as np
+from mne import datasets, Annotations
+from mne.io import read_raw
+
+
+class TestEpochs(unittest.TestCase):
+    def setUp(self) -> None:
+        self.raw_file_path = os.path.join(datasets.sample.data_path(), 'MEG',
+                                          'sample', 'sample_audvis_raw.fif')
+
+        self.raw = read_raw(fname=self.raw_file_path)
+
+    def test_create_epochs_without_annotations(self):
+        from eeg_preprocessing.utils.epochs import create_epochs
+        from eeg_preprocessing.utils.config import settings
+
+        settings['epochs']['duration'] = 2
+
+        epochs = create_epochs(self.raw)
+        assert (epochs.events[1][0] - epochs.events[0][0]) == int(
+            epochs.info['sfreq'] * settings['epochs']['duration'])
+
+        # check epochs are not overlapping
+        n_epochs = epochs.get_data().shape[0]
+        for epoch_num in range(n_epochs - 1):
+            assert not np.array_equal(
+                epochs.get_data()[epoch_num, ...],
+                epochs.get_data()[epoch_num + 1, ...])
+
+        assert int(len(self.raw) / self.raw.info['sfreq'] / 2) == len(epochs)
+        assert epochs.event_id == {'1': 1}
+
+    def test_get_events_from_annotations(self):
+        from eeg_preprocessing.utils.epochs import get_events_from_annotations
+        from eeg_preprocessing.utils.config import settings
+
+        settings['epochs']['duration'] = 1
+
+        annotations = Annotations(onset=[3, 10, 10.5, 11, 22],
+                                  duration=[1, 0.5, 0.25, 1, 1],
+                                  description=[1, 2, 3, 1, 2])
+
+        self.raw.set_annotations(annotations)
+
+        events = get_events_from_annotations(self.raw)
+
+        assert len(events) == 21
+        assert len(events[events[..., 2] == 1]) == 18
+        assert len(events[events[..., 2] == 2]) == 2
+        assert len(events[events[..., 2] == 3]) == 1
+
+    def test_create_epochs_with_annotations(self):
+        from eeg_preprocessing.utils.epochs import create_epochs
+        from eeg_preprocessing.utils.config import settings
+
+        settings['epochs']['duration'] = 1
+
+        annotations = Annotations(onset=[3, 10, 10.5, 11, 22],
+                                  duration=[1, 0.5, 0.25, 1, 1],
+                                  description=[1, 2, 3, 1, 2])
+
+        self.raw.set_annotations(annotations)
+
+        epochs = create_epochs(self.raw)
+
+        # check epochs are not overlapping
+        n_epochs = epochs.get_data().shape[0]
+        for epoch_num in range(n_epochs - 1):
+            assert not np.array_equal(
+                epochs.get_data()[epoch_num, ...],
+                epochs.get_data()[epoch_num + 1, ...])
+
+        assert len(epochs) == 21
+        assert epochs.event_id == {'1': 1, '2': 2, '3': 3}
+        assert len(epochs[0]) == 1
---
+++ eeg_preprocessing/tests/test_utils_raw.py
@@ -0,0 +1,42 @@
+import os
+import unittest
+from mne import datasets
+
+
+class TestRaw(unittest.TestCase):
+    def setUp(self) -> None:
+        self.raw_file_path = os.path.join(datasets.sample.data_path(), 'MEG',
+                                          'sample', 'sample_audvis_raw.fif')
+
+    def test_read_raw_measurement(self) -> None:
+        from eeg_preprocessing.utils.raw import read_raw_measurement
+
+        raw = read_raw_measurement(raw_file_path=self.raw_file_path)
+        assert raw.info['fid'] == 'sample_audvis_raw'
+        assert bool(raw.get_montage())
+
+    def test_filter_raw(self) -> None:
+        from mne.io import read_raw
+        from eeg_preprocessing.utils.raw import filter_raw
+        from eeg_preprocessing.utils.config import settings
+
+        settings['bandpass_filter']['low_freq'] = 1
+        settings['bandpass_filter']['high_freq'] = 30
+
+        raw = read_raw(fname=self.raw_file_path)
+
+        raw_filtered = filter_raw(raw)
+
+        assert raw_filtered.info['highpass'] == 1.0
+        assert raw_filtered.info['lowpass'] == 30.0
+
+        settings['bandpass_filter']['low_freq'] = 5
+        settings['bandpass_filter']['high_freq'] = 45
+
+        raw_filtered = filter_raw(raw)
+
+        assert raw_filtered.info['highpass'] == 5.0
+        assert raw_filtered.info['lowpass'] == 45.0
+
+        assert raw.info['highpass'] != raw_filtered.info['highpass']
+        assert raw.info['lowpass'] != raw_filtered.info['lowpass']

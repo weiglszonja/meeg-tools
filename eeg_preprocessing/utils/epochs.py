4b4cc08b57bc8a9ebbc78f8c16db1644368a3125
weiglszonja <wszonja@gmail.com>
2021-08-18 21:03:57 +0200

    remove preload from epochs creation

--- eeg_preprocessing/utils/epochs.py
+++ eeg_preprocessing/utils/epochs.py
@@ -74,6 +74,65 @@ def create_epochs(raw: Raw) -> Epochs:
                     baseline=None,
                     tmin=0.,
                     tmax=epoch_duration_in_seconds, #- (1 / raw.info['sfreq']
-                    preload=True)
+                    preload=False)
 
     return epochs
+
+
+
+# def create_epochs_from_stimulus_intervals(raw: Raw,
+#                                           intervals: List[tuple]) -> Epochs:
+#     """
+#     Create non-overlapping epochs from stimulus intervals i.e. pairs of
+#     triggers in the data that define a longer period (e.g. resting).
+#     Intervals should be supplied as lists of tuples, where the first element
+#     of each tuple denotes the start time of an event and the second element
+#     the end time of the event (e.g. intervals=[(83, 84), (87, 88)]).
+#     Parameters
+#     ----------
+#     raw
+#     intervals
+#
+#     Returns
+#     -------
+#     Epochs instance
+#     """
+#     events, _ = events_from_annotations(raw)
+#
+#     if int(raw.first_samp) != 0:
+#         events[..., 0] = events[..., 0] - raw.first_samp
+#
+#     epoch_duration = settings['epochs']['duration']
+#     intervals_epoch_events = []
+#
+#     for interval in intervals:
+#         interval_events = events[np.isin(events[..., 2], interval)]
+#         t_min = interval_events[0][0] / raw.info['sfreq']
+#         t_max = interval_events[1][0] / raw.info['sfreq']
+#
+#         raw_segment = raw.copy().crop(tmin=t_min,
+#                                       tmax=t_max,
+#                                       include_tmax=True)
+#
+#         epoch_id = interval_events[0][2]
+#         epoch_events = make_fixed_length_events(raw_segment,
+#                                                 id=int(epoch_id),
+#                                                 first_samp=False,
+#                                                 duration=epoch_duration)
+#
+#         epoch_events[..., 0] = epoch_events[..., 0] + interval_events[0][0]
+#         intervals_epoch_events.append(epoch_events)
+#
+#     interval_events_array = np.concatenate(intervals_epoch_events, axis=0)
+#
+#     epochs = Epochs(raw=raw,
+#                     events=interval_events_array,
+#                     picks='all',
+#                     event_id=list(np.unique(interval_events_array[..., 2])),
+#                     baseline=None,
+#                     tmin=0.,
+#                     tmax=epoch_duration - (1 / raw.info['sfreq']),
+#                     preload=True)
+#
+#     return epochs
+

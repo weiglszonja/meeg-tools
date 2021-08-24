6599ad20f720f5a032aec022d8a12fe877a99898
weiglszonja <wszonja@gmail.com>
2021-08-24 17:23:52 +0200

    add threshold params to config

--- meeg_tools/utils/config.py
+++ meeg_tools/utils/config.py
@@ -1,3 +1,5 @@
 settings = dict(bandpass_filter=dict(low_freq=0.5, high_freq=45),
                 epochs=dict(start_time=0., end_time=1.0, duration=1),
-                ica=dict(n_components=32, method='infomax', decim=None))
+                ica=dict(n_components=32, method='infomax', decim=None),
+                autoreject=dict(threshold=0.15),
+                ransac=dict(threshold=0.2))

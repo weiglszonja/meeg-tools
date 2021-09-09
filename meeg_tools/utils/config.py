8ed6af76945120525712ca657a57313e8b92aa90
weiglszonja <wszonja@gmail.com>
2021-09-09 19:26:15 +0200

    extend configuration file with tfr params

--- meeg_tools/utils/config.py
+++ meeg_tools/utils/config.py
@@ -2,3 +2,10 @@ settings = dict(bandpass_filter=dict(low_freq=0.5, high_freq=45),
                 epochs=dict(start_time=0., end_time=1.0, duration=1),
                 ica=dict(n_components=32, method='infomax', decim=None),
                 autoreject=dict(threshold=0.15))
+
+tfr = dict(morlet=dict(fmin=4,
+                       fmax=45,
+                       step=30,
+                       decim=1),
+           baseline=dict(range=(-0.2, -0.1),
+                         mode='mean'))
